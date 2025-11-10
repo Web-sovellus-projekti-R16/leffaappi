2eece324ac580e5cfd21813acbf2b794407c2e21
Joel Lamsijärvi <joel.lamsijarvi@live.com>
2025-11-10 15:55:51 +0200

    SQL lisätty

---
+++ api/LeffaAppiDb.sql
@@ -0,0 +1,45 @@
+drop table if exists account cascade;
+drop table if exists account_group cascade;
+drop table if exists "group" cascade;
+drop table if exists movie cascade;
+drop table if exists account_movie cascade;
+
+create table account (
+	id serial primary key,
+	email varchar(255) unique not null,
+	password_hash varchar(255) not null
+);
+
+create table "group" (
+	id serial primary key,
+	name varchar(255) not null,
+	description text,
+	owner_email varchar(255)
+);
+
+create table account_group (
+	id serial primary key,
+	account_id int not null,
+	group_id int not null,
+	foreign key (account_id) references account(id) on delete cascade,
+	foreign key (group_id) references "group"(id) on delete cascade
+);
+
+create table movie (
+	id serial primary key,
+	title varchar(255) not null,
+	genre varchar(255),
+	overview text,
+	tmdb_id int not null,
+	poster_path varchar(100)
+);
+
+create table account_movie (
+	id serial primary key,
+	account_id int not null,
+	movie_id int not null,
+	grade int,
+	review text,
+	foreign key (account_id) references account(id) on delete cascade,
+	foreign key (movie_id) references movie(id) on delete cascade
+);
