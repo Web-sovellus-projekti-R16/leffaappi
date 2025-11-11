859937ae1ccdb8bc1cf8dd20f298d3d01baebc38
Joel Lamsijärvi <joel.lamsijarvi@live.com>
2025-11-11 13:16:05 +0200

    teatterissa nyt

---
+++ api/src/helpers/tmdbService.js
@@ -0,0 +1,150 @@
+import axios from 'axios'
+import dotenv from 'dotenv'
+dotenv.config()
+
+let genreCache = null
+
+export async function getCurrentlyOnTheatres() {
+    try {
+        const response = await axios.get(`${process.env.TMDB_BASE_URL}/movie/now_playing`, {
+          headers: {
+            'Authorization': `Bearer ${process.env.TMDB_BEARER}`,
+            'Accept': 'application/json'
+          }
+        });
+
+        const results = response.data.results || [];
+
+        return results.slice(0, 5).map(movie => ({
+            tmdb_id: movie.id,
+            title: movie.title,
+            overview: movie.overview,
+            genre: null,
+            poster_path: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : null
+        }));
+    } catch (err) {
+        console.error('TMDB fetch currently on theatres failed:', err.message);
+    }
+}
+
+export async function searchMovie(title) {
+    try {
+        const response = await axios.get(`${process.env.TMDB_BASE_URL}/search/movie`, {
+          headers: {
+            'Authorization': `Bearer ${process.env.TMDB_BEARER}`,
+            'Accept': 'application/json'
+          },
+          params: { query: title }
+        })
+        const movie = response.data.results[0]
+        if (!movie) return null
+
+        const genres = await fetchGenres()
+        const genreObj = genres.find(g => g.id === movie.genre_ids?.[0])
+        
+        const posterPath = await fetchImage(movie.id)
+
+        return {
+            tmdb_id: movie.id,
+            title: movie.title,
+            overview: movie.overview,
+            genre: genreObj ? genreObj.name : null,
+            poster_path: posterPath ? `https://image.tmdb.org/t/p/w500${posterPath}` : null
+        }
+    } catch (err) {
+        console.error('TMDB search by title fetch failed:', err.message)
+    }
+}
+
+export async function searchMoviesByGenre(genreName) {
+    try {
+        const genres = await fetchGenres()
+        const genreObj = genres.find(g => g.name.toLowerCase() === genreName.toLowerCase())
+        if (!genreObj) return []
+
+        const response = await axios.get(`${process.env.TMDB_BASE_URL}/discover/movie`, {
+          headers: {
+            'Authorization': `Bearer ${process.env.TMDB_BEARER}`,
+            'Accept': 'application/json'
+          },
+          params: { with_genres: genreObj.id }
+        })
+        const results = response.data.results || []
+        return results.map(movie => ({
+            tmdb_id: movie.id,
+            title: movie.title,
+            overview: movie.overview,
+            genre: genreName,
+            poster_path: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : null
+        }))
+    } catch (err) {
+        console.error('TMDB genre search failed:', err.message)
+        return []
+    }
+}
+
+export async function searchMoviesByActor(actorName) {
+    try {
+        const response = await axios.get(`${process.env.TMDB_BASE_URL}/search/person`, {
+          headers: {
+            'Authorization': `Bearer ${process.env.TMDB_BEARER}`,
+            'Accept': 'application/json'
+          },
+          params: { query: actorName }
+        })
+
+        const person = response.data.results?.[0]
+        if (!person || !person.known_for) return []
+
+        return person.known_for.map(movie => ({
+            tmdb_id: movie.id,
+            title: movie.title,
+            overview: movie.overview,
+            genre: null,
+            poster_path: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.cposter_path}` : null
+        }))
+    } catch (err) {
+        console.error('TMDB actor search failed:', err.message)
+        return []
+    }
+}
+
+async function fetchGenres() {
+  if (genreCache) return genreCache
+  try {
+    const response = await axios.get(`${process.env.TMDB_BASE_URL}/genre/movie/list`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.TMDB_BEARER}`,
+        'Accept': 'application/json'
+      }
+    })
+    genreCache = response.data.genres
+    return genreCache
+  } catch (err) {
+    console.log('Failed to fetch genres from TMDB:', err.message)
+  }
+}
+
+async function fetchImage(id) {
+  try {
+    const response = await axios.get(`${process.env.TMDB_BASE_URL}/movie/${id}/images`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.TMDB_BEARER}`,
+        'Accept': 'application/json'
+      }
+    })
+
+    const posters = response.data.posters
+    if (posters && posters.length > 0) {
+      return posters[0].file_path
+    }
+    const backdrops = response.data.backdrops
+    if (backdrops && posters.length > 0) {
+      return backdrops[0].file_path
+    }
+    return null
+  } catch (err) {
+    console.log('Failed to fetch images from TMDB:', err.message)
+    return null
+  }
+}
--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
-import bookRouter from "./routers/book_router.js";
 import movieRouter from "./routers/movie_router.js";
 
 const app = express();
@@ -13,7 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/movies", movieRouter);
-app.use("/book", bookRouter);
 
 app.get("/", async (req, res) => {
   res.send("Postgres API esimerkki");
