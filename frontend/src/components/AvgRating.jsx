5e6526aed414831dbafede944eacfb7915997a51
tupa4kka1 <niilo.tanhuala@gmail.com>
2025-11-27 23:20:36 +0200

    puolikkaat tähdet kartanolle

--- frontend/src/components/AvgRating.jsx
+++ frontend/src/components/AvgRating.jsx
@@ -3,20 +3,17 @@ import "./AvgRating.css"
 export default function AvgRating({ reviews }) {
   if (!reviews || reviews.length === 0) return null
 
-  const average = (
-    reviews.reduce((sum, r) => sum + r.rating, 0) / reviews.length
-  ).toFixed(1)
+  const average1 = reviews.reduce((sum, r) => sum + r.rating, 0) / reviews.length
+  const average = Math.round(average1)
 
-  const full = Math.floor(average)
-  const half = average - full >= 0.5
-  const empty = 5 - full - (half ? 1 : 0)
+  const full = average
+  const empty = 5 - full
 
   return (
     <div className="avg-rating-component">
       <div className="avg-stars">
-        {Array(full).fill(0).map((_, i) => <span key={"f"+i}>★</span>)}
-        {half && <span className="half">★</span>}
-        {Array(empty).fill(0).map((_, i) => <span key={"e"+i} className="empty">★</span>)}
+        {Array(full).fill(0).map((_, i) => (<span key={"f" + i} className="star gold">★</span>))}
+        {Array(empty).fill(0).map((_, i) => (<span key={"e" + i} className="star gray">★</span>))}
       </div>
     </div>
   )
