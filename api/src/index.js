45f2385b2a019cab5308cc57af0fe88f286d31e3
マーク <80691235+Mahiro-dev@users.noreply.github.com>
2025-12-01 18:46:57 +0200

    Add files via upload

--- api/src/index.js
+++ api/src/index.js
@@ -6,19 +6,25 @@ import reviewRouter from "./routers/review_router.js"
 
 import movieRouter from "./routers/movie_router.js";
 import accountRouter from "./routers/account_router.js";
+import groupRouter from "./routers/group_router.js";
+
+
+console.log("JWT_SECRET at boot:", process.env.JWT_SECRET);
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors({
-  origin: process.env.REACT_URL,
+  origin: "http://localhost:5173",
   credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/movies", movieRouter);
+app.use("/group", groupRouter);
 app.use("/account", accountRouter);
 
 app.use("/reviews", reviewRouter)
