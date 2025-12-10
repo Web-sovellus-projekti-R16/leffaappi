7a74fa2ff8ce7541e2c217ffeae2065fe611970b
Joel Lamsijärvi <joel.lamsijarvi@live.com>
2025-12-10 17:44:29 +0200

    Korjaus: Nyt teatterit tyhjät arvostelut

--- frontend/src/components/nowPlaying.jsx
+++ frontend/src/components/nowPlaying.jsx
@@ -7,18 +7,20 @@ export default function NowPlaying() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [ratings, setRatings] = useState({})
-
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function load() {
       try {
         const res = await fetch(`${import.meta.env.VITE_API_URL}/movies/nowplaying`);
+        if (!res.ok) throw new Error("Failed to fetch now playing movies");
         const data = await res.json();
         setMovies(data)
         data.forEach(movie => loadRating(movie.tmdb_id))
 
       } catch (err) {
         console.error("NowPlaying failed:", err);
+        setError("Failed to load movies. Try again later.")
       } finally {
         setLoading(false);
       }
@@ -26,15 +28,16 @@ export default function NowPlaying() {
     load();
   }, []);
 
-  if (loading) return <p>Loading...</p>;
-
+  
   async function loadRating(tmdb_id) {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/reviews/movie/tmdb/${tmdb_id}`)
-  if (!res.ok) return
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/reviews/movie/tmdb/${tmdb_id}`)
+    if (!res.ok) return
     const data = await res.json()
     setRatings(prev => ({ ...prev, [tmdb_id]: data }))
   }
-
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p className="error">{error}</p>
 
   return (
     <section>
@@ -45,7 +48,7 @@ export default function NowPlaying() {
           <Link to={`/movie/${m.tmdb_id}`} key={m.tmdb_id} className="movie-card-link">
             <div className="movie-card">
               <h3>{m.title}</h3>
-              <AvgRating reviews={ratings[m.tmdb_id]} />
+              <AvgRating reviews={ratings[m.tmdb_id] || []} />
               {m.poster_path && (
                 <img src={m.poster_path} alt={m.title} />
               )}
