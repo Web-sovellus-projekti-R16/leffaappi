d4467bdc8457da65777b2c76c6a8b027b3165e4e
tupa4kka1 <niilo.tanhuala@gmail.com>
2025-12-14 06:28:44 +0200

    vähän testien testailua

--- api/src/index.test.js
+++ api/src/index.test.js
@@ -97,4 +97,92 @@ describe("API Functional Tests", () => {
         const data = await protectedRes.json();
         expect(data.user).to.have.property("email", testUser.email);
     });
+    it("should logout", async () => {
+        const loginRes = await fetch("http://localhost:3001/account/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(testUser),
+        });
+
+        const cookies = loginRes.headers.get("set-cookie");
+
+        const logoutRes = await fetch("http://localhost:3001/account/logout", {
+            method: "GET",
+            headers: {
+                Cookie: cookies
+            }
+        });
+
+        expect(logoutRes.status).to.equal(200);
+
+        const body = await logoutRes.json();
+        expect(body.message).to.equal("Logout succesful");
+    });
+
+    it("should delete account when confirmation is done", async () => {
+        const loginRes = await fetch("http://localhost:3001/account/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(testUser),
+        });
+
+        const { token } = await loginRes.json();
+
+        const deleteRes = await fetch("http://localhost:3001/account/delete", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({ confirmation: "delete-my-account" }) //////////////////////////////////////////
+        });
+
+        expect(deleteRes.status).to.equal(200);
+
+        const body = await deleteRes.json();
+        expect(body.message).to.include("flagged as deleted");
+    });
+
+    it("should not delete account if confirmation is wrong", async () => { ////////////////////////////////////////////
+        const loginRes = await fetch("http://localhost:3001/account/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(testUser),
+        });
+
+        const { token } = await loginRes.json();
+
+        const deleteRes = await fetch("http://localhost:3001/account/delete", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({ confirmation: "hahahahahahahahahahahahahahahah" }) /////////////
+        });
+
+        expect(deleteRes.status).to.equal(400);
+    });
+
+
+
+    it("should get reviews for a movie", async () => {
+        const response = await fetch("http://localhost:3001/reviews/movie/tmdb/550");
+        expect(response.status).to.equal(200);
+
+        const body = await response.json();
+        expect(body).to.be.an("array");
+    });
+
+    it("should return empty array if movie has no reviews", async () => {
+        const response = await fetch("http://localhost:3001/reviews/movie/tmdb/999999");
+        expect(response.status).to.equal(200);
+
+        const body = await response.json();
+        expect(body).to.be.an("array");
+    });
 });
+
+
+
+
